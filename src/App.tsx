6609a3e4db3ef810fbbc24c66f1cb4d8bb795388
pione30 <g.pionelish30@gmail.com>
2021-08-10 01:17:52 +0900

    introduce useDrag and usePinch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useDrag, usePinch } from "react-use-gesture";
 
 function App() {
+  const [offset, setOffset] = useState<[number, number]>();
+
+  const bindDrag = useDrag((state) => {
+    setOffset(state.offset);
+  });
+
+  const bindPinch = usePinch(
+    (state) => {
+      setOffset(state.offset);
+    },
+    {
+      eventOptions: {
+        passive: false,
+      },
+    }
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div {...bindDrag()} className="draggable">
+          <img
+            src={logo}
+            style={{ width: offset?.[0], height: offset?.[1] }}
+            className="App-logo"
+            alt="logo"
+          />
+        </div>
+        <div {...bindPinch()} className="draggable">
+          <img
+            src={logo}
+            style={{ width: offset?.[0], height: offset?.[1] }}
+            className="App-logo"
+            alt="logo"
+          />
+        </div>
+        {offset && (
+          <div>
+            offset: {offset[0]}, {offset[1]}
+          </div>
+        )}
       </header>
     </div>
   );
